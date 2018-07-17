2f258feb1492156a2915a0419a811cd3baf4efd1
Gabriel Santos <mfkrgabriel@gmail.com>
2018-07-17 17:11:54 -0300

    Continuing with challenge

--- challenge-04/challenge-04.js
+++ challenge-04/challenge-04.js
@@ -66,24 +66,32 @@ carro.mudarCor = function ( color ) {
 /*
 Crie um método chamado `obterCor`, que retorne a cor do carro.
 */
-?
+carro.obterCor = function () {
+	return carro.cor;
+}
 
 /*
 Crie um método chamado `obterModelo` que retorne o modelo do carro.
 */
-?
+carro.obterModelo = function () {
+	return carro.modelo;
+}
 
 /*
 Crie um método chamado `obterMarca` que retorne a marca do carro.
 */
-?
+carro.obterMarca = function () {
+	return carro.marca;
+}
 
 /*
 Crie um método chamado `obterMarcaModelo`, que retorne:
 "Esse carro é um [MARCA] [MODELO]"
 Para retornar os valores de marca e modelo, utilize os métodos criados.
 */
-?
+carro.obterMarcaModelo = function () {
+	return 'Esse carro é um ' + carro.marca + ' ' + carro.modelo;
+}
 
 /*
 Crie um método que irá adicionar pessoas no carro. Esse método terá as
@@ -101,8 +109,22 @@ mostrar quantos assentos ainda podem ser ocupados, com a frase:
 - Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
 citado acima, no lugar de "pessoas".
 */
-?
+carro.adicionarPessoa = function ( num ) {
+  
+  var totalPessoas = carro.quantidadePessoas + num;
+  var assentosRestantes = carro.assentos - carro.quantidadePessoas;
+  var singular = assentosRestantes > 1 ? "pessoas!": "pessoa!"
+  if(carro.quantidadePessoas === carro.assentos && totalPessoas >= carro.assentos) {
+    return "O carro já está lotado!";
+  }
+  if(totalPessoas > assentosRestantes) {
+    return "Só cabem mais " + assentosRestantes + " " + singular;
+  } else {
+    carro.quantidadePessoas += num;
+    return "Já temos " + carro.quantidadePessoas + " pessoas no carro!";
+  }
 
+}
 /*
 Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
 utilize sempre o formato de invocação do método (ou chamada da propriedade),
